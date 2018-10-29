2940010d6464b3a4e0739ef50bc9b054e498cb90
bertholdberthi <43133806+bertholdberthi@users.noreply.github.com>
2018-10-29 13:33:10 +0100

    push

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,91 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the !!!!!!!!!!!!!!!!!
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <section class="todoapp">
+    <header class="header">
+      <h1>Commandes</h1>
+      <input type="text" class="new-todo" placeholder="Ajouter une Commande" v-model="newTodo" @keyup.enter="addTodo">
+
+    </header>
+    <div class="main">
+      <input class="toggle-all" type="checkbox"  v-model="allDone">
+      <ul class="todo-list" id="">
+        <li class="todo" v-for="todo in filteredTodos" :class="{ completed: todo.completed }" :key="todo.id">
+          <div class="view">
+          <input type="checkbox" v-model="todo.completed" class="toggle"> 
+          <label>{{ todo.name }}</label>
+            <button class="destroy" @click.prevent="deleteTodo(todo)"></button>
+            </div>
+        </li> 
+      </ul>
+    </div>
+    <footer class="footer" >  
+      <!-- v-show="hasTodos" -->
+      <span class="todo-count"><strong>{{ remaining }}</strong> Commande (s) </span>
+      <ul class="filters">
+        <li><a href="#" :class="{selected: filter === 'all'}" @click.prevent="filter= 'all'">Toues commandes</a> </li>
+        <li><a href="#" :class="{selected: filter === 'todo'}" @click.prevent="filter= 'todo'">à faire</a> </li>
+        <li><a href="#" :class="{selected: filter === 'done'}" @click.prevent="filter= 'done'">faites</a> </li>
+        </ul>
+      </footer>
+  </section>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String
+  data() {
+    return {
+      el: "#v-for-todos",
+      todos: [
+        {
+          name: "Commande de Text",
+          completed: false
+        }
+      ],
+      newTodo: "",
+      filter: "all"
+    };
+  },
+
+  methods: {
+    addTodo() {
+      this.todos.push({
+        completed: false,
+        name: this.newTodo
+      });
+      this.newTodo = "";
+    },
+    deleteTodo(todo) {
+      alert("z");
+      this.todo = this.filter(i => i !== todo);
+    }
+  },
+  computed: {
+    allDone: {
+      get() {
+        return this.remaining === 0;
+      },
+      set(value) {
+        this.todos.forEach(todo => {
+          todo.completed = value;
+        });
+      }
+    },
+    remaining() {
+      alert("compteur");
+      return this.todos.filter(todo => !todo.completed).lenght;
+    },
+    // hasTodos() {
+    //   return this.todos.lenght > 0;
+    // },
+    filteredTodos() {
+      if (this.filter === "todo") {
+        return this.todos(todo => !todo.completed);
+      } else if (this.filter === "done") {
+        return this.todos(todo => todo.completed);
+      }
+      return this.todos;
+    }
   }
 };
 </script>
+<style src="./vue.css"></style>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
